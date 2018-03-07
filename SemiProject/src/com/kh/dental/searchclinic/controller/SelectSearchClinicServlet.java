24825b810052fa2198d9d916bf28f208cd0efd6f
sosoccdd <sosoccdd22@gmail.com>
2018-03-07 19:02:41 +0900

    서은별 병원찾기 지도 연결

--- SemiProject/src/com/kh/dental/searchclinic/controller/SelectSearchClinicServlet.java
+++ SemiProject/src/com/kh/dental/searchclinic/controller/SelectSearchClinicServlet.java
@@ -1,16 +1,22 @@
 package com.kh.dental.searchclinic.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.kh.dental.searchclinic.model.service.SearchClinicService;
+import com.kh.dental.searchclinic.model.vo.SearchClinic;
+
 /**
  * Servlet implementation class SelectSearchClinicServlet
  */
-@WebServlet("/SelectSearchClinicServlet")
+@WebServlet("/selectsearch.sc")
 public class SelectSearchClinicServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,8 +32,20 @@ public class SelectSearchClinicServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String sido = request.getParameter("sido");
+		String gugun = request.getParameter("gugun");
+		String dong = request.getParameter("dong");
+		
+		System.out.println(sido+","+gugun+","+dong);
+		
+		ArrayList<SearchClinic> list = new SearchClinicService().selectaddress(sido, gugun, dong);
+		
+		//페이지
+		System.out.println("servlet:"+list);
+		
+		
+		
 	}
 
 	/**
--- SemiProject/src/com/kh/dental/searchclinic/model/service/SearchClinicService.java
+++ SemiProject/src/com/kh/dental/searchclinic/model/service/SearchClinicService.java
@@ -1,5 +1,24 @@
 package com.kh.dental.searchclinic.model.service;
 
+import static com.kh.dental.common.JDBCTemplet.*;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import com.kh.dental.searchclinic.model.dao.SearchClinicDao;
+import com.kh.dental.searchclinic.model.vo.SearchClinic;
+
 public class SearchClinicService {
+
+	public ArrayList<SearchClinic> selectaddress(String sido, String gugun, String dong) {
+		Connection con = getConnection();
+		
+		ArrayList<SearchClinic> list = new SearchClinicDao().selectaddress(con, sido, gugun, dong);
+		
+		close(con);
+		
+		
+		return list;
+	}
 
 }
